a253a649447c21dcfa4c6db5ba4d17db253e1df0
elegant-codes <ajakayetolu@gmail.com>
2021-01-06 16:21:51 +0100

    files 1

---
+++ js/questions.js
@@ -0,0 +1,644 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "REM sleep occupies...... % of total sleep time in all ages",
+    answer: "D. 20 - 25%",
+    options: [
+      "A. 20-50%",
+      "B. 25 - 45%",
+      "C. 30 - 45%",
+      "D. 20 - 25%",
+      "E. 45 - 55%"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is true?",
+    answer: "None of the above",
+    options: [
+      "Cholinergic neurons are active during sleep",
+      "cholinergic neurons are inactive during slow wave sleep",
+      "Cholinergic neurons are inactive during NON-REM sleep",
+      "Cholinergic neurons are active in both waking and sleeping periods",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following scientists were the first to investigate the neuronal component regulating the brain sleep - wake mechanism?",
+    answer: "C and D only",
+    options: [
+      "Hans Berger",
+      "Alfred Loomis",
+      "Moruzzi",
+      "Magoun",
+      "C and D only"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The RAS is involved in which of the following?",
+    answer: "All of the above",
+    options: [
+      "activation of the cortex",
+      "maintaining muscle tone of antigravity muscle",
+      "assisting in regulation of heart beat",
+      "All of the above",
+      " A and B only"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following produced the FIRO(Fundamental Interpersonal Relations Orientation) theory to examine group dynamics?",
+    answer: "William Schutz",
+    options: [
+      "Wilfred Bion",
+      "William Schutz",
+      "Bruce Tuckman",
+      "Irvin Yalom",
+      "Hollingshead and Redlich"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which of the following is not true about sociology?",
+    answer: "None of the above",
+    options: [
+      "It is a scientific discipline",
+      "It has many approaches and paradigms",
+      "Concepts are the building blocks",
+      "Looks at society using concepts",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "The Four main methods of sociological research includes the following except ",
+    answer: "outreaches",
+    options: [
+      "Experiments",
+      "Surveys",
+      "Field methods",
+      "outreaches",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Sociology relies on which of the following paradigms to investigate society?",
+    answer: "All of the above",
+    options: [
+      "Theory",
+      "Data",
+      "Analysis",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Sociology as a quest to understand patterns of behaviour is found in which of the following disciplines",
+    answer: "All of the above",
+    options: [
+      "Psychology",
+      "Political science",
+      "Psychiatry",
+      "All of the above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 10,
+    question: "According to Hess, Markson and Stein, sociology is which of the following?",
+    answer: "All of the above",
+    options: [
+      "the systematic study of human behaviour",
+      "the groups to which one belongs",
+      "the societies that human beings create and within which their lives unfold",
+      "All of the above",
+      "A and C only"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Which of the following is true in anxiety disorders?",
+    answer: "All of the above",
+    options: [
+      "poor executive functioning",
+      "Poor neuropsychological performance in attention",
+      "poor performance in memory",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 12,
+    question: "What percentage of depressed patients have cognitive impairment?",
+    answer: "50 - 70%",
+    options: [
+      "50 - 80%",
+      "60 - 90%",
+      "45 - 75%",
+      "50 - 70%",
+      "65 - 85%"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Experts are more likely to use which problem solving technique that are not available to the novice",
+    answer: "Heuristic",
+    options: [
+      "Trial and error",
+      "Algorithm",
+      "Heuristic",
+      "Insight",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 14,
+    question: "Problem-solving strategies include the following except ",
+    answer: "None of the above",
+    options: [
+      "Trial and errors",
+      "Insight",
+      "Algorithm",
+      "Heuristic technique",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 15,
+    question: "The process of reasoning from one or more general statements to reach a logically certain conclusion is",
+    answer: "All of the above",
+    options: [
+      "Deductive reasoning",
+      "Deductive logic",
+      "Logical deduction",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 16,
+    question: "In thinking, we make use of the following?",
+    answer: "All of the above",
+    options: [
+      "concepts",
+      "prototypes",
+      "cores",
+      "All of the above",
+      "B and C only"
+    ]
+  },
+    {
+    numb: 17,
+    question: "Which of the following is true about thinking?",
+    answer: "All of the above",
+    options: [
+      "said to occur in all parts of the brain",
+      "the prefrontal cortex modulates rational thinking and is involved with decision making ",
+      "thinking helps us define and organize experiences",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 18,
+    question: "Which of the following is true about the hippocampus?",
+    answer: "critical in declarative memory",
+    options: [
+      "critical in declarative memory",
+      "Necessary for procedural memory",
+      "essential for memory storage",
+      "damage affects procedural memory",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 19,
+    question: "The hearing functions of the temporal lobe in the non-dominant hemisphere include",
+    answer: "None of the above",
+    options: [
+      "naming of musical scores",
+      "verbal aspect of memory ",
+      "all the semantic aspect music",
+      "All of the above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 20,
+    question: "Basic sensory function of the temporal lobe include the following",
+    answer: "All of the above",
+    options: [
+      "processing auditory input",
+      "Visual object recognition",
+      "Long term storage of sensory input",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 21,
+    question: "Which of the following connections of the temporal lobes is correctly matched with its function",
+    answer: "Medial temporal pathway - -long term memory ",
+    options: [
+      "Hierarchical sensory pathway - - directing movements with respect to auditory information",
+      "Dorsal auditory pathway - - stimulus Categorisation",
+      "Polymodal pathway - - necessary for various aspects of movement control, affect and short-term memory",
+      "Medial temporal pathway - -long term memory ",
+      "Frontal Lobe projection - - stimulus recognition"
+    ]
+  },
+    {
+    numb: 22,
+    question: "Paul D. Maclean expanded the Papez Circuit to include the following",
+    answer: "A and B only",
+    options: [
+      "Prefrontal cortex",
+      "Amygdala",
+      "Dentate cortex",
+      "Posterior Thalamic nucleus",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 23,
+    question: "Lesions in the amygdala can  lead to the following except ",
+    answer: "Hyperorality",
+    options: [
+      "agitation",
+      "Irritability",
+      "Hyperorality",
+      "paranoia",
+      "psychosis"
+    ]
+  },
+    {
+    numb: 24,
+    question: "Which of the following is true about the Primary auditory cortex?",
+    answer: "All of the above",
+    options: [
+      "Brodmann areas 41 and 42",
+      "transverse gyri",
+      "Heschl's gyri",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+  {
+    numb: 25,
+    question: "The medial temporal region (Limbic Cortex) includes ",
+    answer: "All of the above",
+    options: [
+      "Entorhinal cortex",
+      "Subiculum",
+      "Perirhinal cortex",
+      "All of the above",
+      "A and C only"
+    ]
+  },
+    {
+    numb: 26,
+    question: "Concerning the temporal lobes, which of the following is true?",
+    answer: "approx 17% of the volume of cerebral cortex",
+    options: [
+      "lateral to the occipital lobes",
+      "anterior to the frontal and parietal lobes",
+      "Inferior to the Lateral sulcus",
+      "approx 17% of the volume of cerebral cortex",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 27,
+    question: "Components of uncinate fits include",
+    answer: "All of the above",
+    options: [
+      "olfactory aura",
+      "involuntary movements",
+      "sensations of deja vu and jamais vu",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 28,
+    question: "KluverBucy syndrome comprises?",
+    answer: "All of the above",
+    options: [
+      "Visual agnosia",
+      "Hyperorality",
+      "Hypersexuality",
+      "All of the above",
+      "A and C only"
+    ]
+  },
+    {
+    numb: 29,
+    question: "The theory of the triune brain was developed by",
+    answer: "Paul D. Maclean",
+    options: [
+      "Scoville and Milner",
+      "Paul Kluver and Heinrich Bucy",
+      "James Papez",
+      "Paul Broca",
+      "Paul D. Maclean"
+    ]
+  },
+    {
+    numb: 30,
+    question: "A gene or sequence of DNA that occurs in two or more common forms is",
+    answer: "polymorphism",
+    options: [
+      "Restriction Fragment Length Polymorphism",
+      "recombinant DNA",
+      "polymorphism",
+      "Alleles",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 31,
+    question: "Techniques in molecular genetics include the following ",
+    answer: "All of the above",
+    options: [
+      "molecular cloning",
+      "Gene tracking",
+      "Direct gene analysis",
+      "Linkage studies",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 32,
+    question: "Higher-level organization of nucleic acids in chromatin and histones refers to which structure of the human genome",
+    answer: "Quaternary",
+    options: [
+      "Primary",
+      "Secondary",
+      "Tertiary",
+      "Quaternary",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 33,
+    question: "Which of the following refers to the coding regions in the human genome ",
+    answer: "Exons",
+    options: [
+      "introns",
+      "Exons",
+      "histones",
+      "All of the above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 34,
+    question: "Which of the following is true about mitochondrial inheritance?",
+    answer: "Law of segregation",
+    options: [
+      "Law of independent assortment",
+      "Law of dominance",
+      "Law of segregation",
+      "Law of inheritability",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 35,
+    question: "Mendel's first law is also known as",
+    answer: "All of the above",
+    options: [
+      "Psychology",
+      "Political science",
+      "Psychiatry",
+      "All of the above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 36,
+    question: "Which of the following published the theory of use and disuse?",
+    answer: "Jean Lamarck",
+    options: [
+      "Charles Darwin",
+      "Gregor Mendel",
+      "Jean Lamarck",
+      "Thomas Rutherford ",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 37,
+    question: "Examples of perceptual disturbance include the following except ",
+    answer: "None of the above",
+    options: [
+      "Capgras syndrome",
+      "Fregoli",
+      "Intermetamorphosis",
+      "Delusion of subjective doubles",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 38,
+    question: "The following is true about the trichromatic theory",
+    answer: "Combination of the three colours create other colours",
+    options: [
+      "it is based in the work of Thomas Young alone",
+      "The retina has three types of colour receptor cones: Blue, Green and yellow",
+      "Colour blindness have normal Green sensory cones",
+      "Combination of the three colours create other colours",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 39,
+    question: "The Four possible stimulus/response situations include",
+    answer: "All of the above",
+    options: [
+      "Hit",
+      "Miss",
+      "False alarm",
+      "Correct rejection",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 40,
+    question: "Which of the following is true according to Weber's law",
+    answer: "The smaller the stimulus, the smaller the change required to notice anything different",
+    options: [
+      "The larger a stimulus, the smaller the change required to notice anything different",
+      "The weaker a stimulus, the larger the change required to notice anything different",
+      "The smaller the stimulus, the larger the change required to notice any difference",
+      "The smaller the stimulus, the smaller the change required to notice anything different",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 41,
+    question: "Diminished sensitivity as a result of constant stimulation is",
+    answer: "Adaptation",
+    options: [
+      "Transduction",
+      "Adaptation",
+      "Activation",
+      "Acclimatization",
+      "Sensation"
+    ]
+  },
+    {
+    numb: 42,
+    question: "The conversion of stimulus energy into neural impulses by the sensory receptors is referred to as",
+    answer: "Transduction",
+    options: [
+      "Adaptation",
+      "Sensation",
+      "Transduction",
+      "Transmission",
+      "C and D only"
+    ]
+  },
+    {
+    numb: 43,
+    question: "Which of the following is the basic building blocks of perception",
+    answer: "All of the above",
+    options: [
+      "Transduction",
+      "Sensation",
+      "Stimuli",
+      "Adaptation",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 44,
+    question: "Contributions of Anthropology to Psychiatry includes the following",
+    answer: "All of the above",
+    options: [
+      "emphasizing the relevance of social environment in the diagnosis, treatment and prognosis of Psychiatric ailments",
+      "creating concepts and methods that psychiatrists are able to apply",
+      "the development of socially optimal Psychiatric programmes",
+      "secures the development of politics and programmes implying close interconnectedness of social conditions and mental health",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 45,
+    question: "The reification of a nosological category developed for a particular cultural group that is then applied to members of another culture for whom it lacks coherence and its validity has not been established",
+    answer: "Categorical fallacy",
+    options: [
+      "Categorical fallacy",
+      "Transcultural fallacy",
+      "Anthropological Categorisation",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 46,
+    question: "Basic sensory function of the temporal lobe include the following",
+    answer: "All of the above",
+    options: [
+      "processing auditory input",
+      "Visual object recognition",
+      "Long term storage of sensory input",
+      "All of the above",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 21,
+    question: "Which of the following connections of the temporal lobes is correctly matched with its function",
+    answer: "Medial temporal pathway - -long term memory ",
+    options: [
+      "Hierarchical sensory pathway - - directing movements with respect to auditory information",
+      "Dorsal auditory pathway - - stimulus Categorisation",
+      "Polymodal pathway - - necessary for various aspects of movement control, affect and short-term memory",
+      "Medial temporal pathway - -long term memory ",
+      "Frontal Lobe projection - - stimulus recognition"
+    ]
+  },
+    {
+    numb: 22,
+    question: "Paul D. Maclean expanded the Papez Circuit to include the following",
+    answer: "A and B only",
+    options: [
+      "Prefrontal cortex",
+      "Amygdala",
+      "Dentate cortex",
+      "Posterior Thalamic nucleus",
+      "A and B only"
+    ]
+  },
+    {
+    numb: 23,
+    question: "Lesions in the amygdala can  lead to the following except ",
+    answer: "Hyperorality",
+    options: [
+      "agitation",
+      "Irritability",
+      "Hyperorality",
+      "paranoia",
+      "psychosis"
+    ]
+  },
+    {
+    numb: 50,
+    question: "The Four Anthropological Questions for Psychiatry includes",
+    answer: "All of the above",
+    options: [
+      "To what extent do Psychiatric disorders differ in different societies?",
+      "What place does translation have in cross-cultural research?",
+      "Does the standard approach to cross-cultural research in psychiatry commit a 'category fallacy'?",
+      "All of the above",
+      "A and C only"
+    ]
+  },
+  {
+    numb: 25,
+    question: "The medial temporal region (Limbic Cortex) includes ",
+    answer: "All of the above",
+    options: [
+      "Entorhinal cortex",
+      "Subiculum",
+      "Perirhinal cortex",
+      "All of the above",
+      "A and C only"
+    ]
+  },
+    {
+    numb: 26,
+    question: "Concerning the temporal lobes, which of the following is true?",
+    answer: "approx 17% of the volume of cerebral cortex",
+    options: [
+      "lateral to the occipital lobes",
+      "anterior to the frontal and parietal lobes",
+      "Inferior to the Lateral sulcus",
+      "approx 17% of the volume of cerebral cortex",
+      "All of the above"
+    ]
+  },
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
